40be0ad6b61f2026218dece46e386835ed610498
clivepato93 <clivetoniato1@hotmail.com>
2022-04-28 19:31:43 +0100

    updated markup

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.addEventListener('resize',function(){
     console.log(this.window.innerWidth)
 })
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click',function (e) {
+    e.preventDefault();
+    alert('💩')
+})
