791e332462f29425cfc79db9a791f2753ac4856b
clivepato93 <clivetoniato1@hotmail.com>
2022-04-28 22:31:30 +0100

    email validation complete

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ window.addEventListener('resize',function(){
     console.log(this.window.innerWidth)
 })
 
+const form = document.querySelector('.form');
 const button = document.querySelector('.btn');
+const emailBox = document.querySelector('#inputBox');
+const warning = document.querySelector('.warning');
 
+console.log(emailBox);
 button.addEventListener('click',function (e) {
     e.preventDefault();
-    alert('💩')
+    const email = emailBox.value;
+    // alert(emailBox.textContent)
+    // alert('💩')
+    if( /@/.test(email)) {
+       // return ;
+       console.log('logging from true')
+       warning.classList.add('hidden')
+    }
+    
+    
+    else {
+        console.log('logging from false')
+        warning.classList.remove('hidden') 
+    }
 })
